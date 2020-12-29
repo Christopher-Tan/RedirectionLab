0741b790aaab6a27f764aac16e0129389fb2aa21
Christopher Tan <ctan20@stuy.edu>
2020-12-29 09:03:41 -0500

    Added and Tested Pig Latin 1 Method

--- PigLatin.java
+++ PigLatin.java
@@ -1,3 +1,16 @@
+import java.util.Arrays;
 public class PigLatin {
-    
+    public static String pigLatinSimple(String s) {
+        s = s.toLowerCase();
+        if (Arrays.asList(new String[]{"a","e","i","o","u"}).contains(s.substring(0,1))) {
+            return s + "hay";
+        }
+        return s.substring(1,s.length()) + s.substring(0,1) + "ay";
+    }
+    public static void main(String[] args) {
+        System.out.println(pigLatinSimple("mock"));
+        System.out.println(pigLatinSimple("pie"));
+        System.out.println(pigLatinSimple("david"));
+        System.out.println(pigLatinSimple("aaron"));
+    }
 }
