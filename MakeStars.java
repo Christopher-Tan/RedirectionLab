459ae7598d5a085f7a9d31f9ca547bdb9bb16033
Christopher Tan <ctan20@stuy.edu>
2020-12-29 08:46:26 -0500

    Created and tested MakeStars class

--- MakeStars.java
+++ MakeStars.java
@@ -1,2 +1,17 @@
+import java.util.*;
 public class MakeStars {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        while (n.hasNextLine()) {
+            Scanner m = new Scanner(n.nextLine());
+            while (m.hasNext()) {
+                String s = m.next();
+                for (int i = 0; i < s.length(); i++) {
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
 }
